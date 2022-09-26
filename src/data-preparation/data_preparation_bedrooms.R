3e383f3dfcd0ee5a0c6e7af29b2ebdfce114307b
MerelvStekelenburg <m.vanstekelenburg@tilburguniversity.edu>
2022-09-26 22:07:39 +0200

    ean impute NAs beds and bedrooms

--- src/data-preparation/data_preparation_bedrooms.R
+++ src/data-preparation/data_preparation_bedrooms.R
@@ -13,3 +13,12 @@ dec_s$bedrooms<- ifelse(dec_s$shared_room_room_type == 1 & is.na(dec_s$bedrooms)
 mar_s$bedrooms<- ifelse(mar_s$shared_room_room_type == 1 & is.na(mar_s$bedrooms), 1, mar_s$bedrooms)
 june_s$bedrooms<- ifelse(june_s$shared_room_room_type == 1 & is.na(june_s$bedrooms), 1, june_s$bedrooms)
 
+# Remaining NAs are changed into the mean of bedrooms
+dec_s$bedrooms[is.na(dec_s$bedrooms)] <- mean(dec_s$bedrooms,na.rm = TRUE)
+mar_s$bedrooms[is.na(mar_s$bedrooms)] <- mean(mar_s$bedrooms,na.rm = TRUE)
+june_s$bedrooms[is.na(june_s$bedrooms)] <- mean(june_s$bedrooms,na.rm = TRUE)
+
+#check if there are NA's left
+summary(dec_s$bedrooms)
+summary(june_s$bedrooms)
+summary(mar_s$bedrooms)
