0de746c0933a7f41cb43c97052a04969b1322048
Ana Luca <60931541+Ana042@users.noreply.github.com>
2022-10-10 19:29:44 +0200

    Update regression_analysis.R
    complete analysis for june data set

--- src/analysis/regression_analysis.R
+++ src/analysis/regression_analysis.R
@@ -1,18 +1,17 @@
+# merge june listings and calendar
+# Rename listings_id to id for merging
+calendar_j_s<- calendar_j_s %>% rename(id = listing_id)
+
+# Merge the datasets
+merged_june <- merge(june_s,calendar_j_s)
+
+
+#divide data per seasons 
+#summer: May-Oct
+#winter: Nov-April
+merged_june$winter <- ifelse((merged_june$date >= as.Date("2022-11-01") & merged_june$date < as.Date("2023-04-01")), 1, 0) 
+
+
 # Linear Regression june
-merged_june_lm <- lm(price_n ~ bathrooms + accommodates + beds + bedrooms + review_scores_rating + bathroom_shared + hawaii_neighbourhood + kauai_neighbourhood + maui_neighbourhood +entire_home_apt_room_type + private_room_room_type + shared_room_room_type + instant_bookable_dummy + time_diff, merged_june, na.action = na.omit)
+merged_june_lm <- lm(price_n ~ bathrooms + accommodates + beds + bedrooms + review_scores_rating + bathroom_shared + hawaii_neighbourhood + kauai_neighbourhood + maui_neighbourhood +entire_home_apt_room_type + private_room_room_type + shared_room_room_type + instant_bookable_dummy + time_diff + winter, merged_june, na.action = na.omit)
 summary(merged_june_lm)
-
-# Linear Regression december
-merged_dec_lm <- lm(price_n ~ bathrooms + accommodates + beds + bedrooms + review_scores_rating + bathroom_shared  + hawaii_neighbourhood + kauai_neighbourhood + maui_neighbourhood +entire_home_apt_room_type + private_room_room_type + shared_room_room_type + instant_bookable_dummy + time_diff, merged_dec, na.action = na.omit)
-summary(merged_dec_lm)
-
-# Linear Regression march
-merged_mar_lm <- lm(price_n ~ bathrooms + accommodates + beds + bedrooms + review_scores_rating + bathroom_shared + hawaii_neighbourhood + kauai_neighbourhood + maui_neighbourhood +entire_home_apt_room_type + private_room_room_type + shared_room_room_type + instant_bookable_dummy + time_diff, merged_mar, na.action = na.omit)
-summary(merged_mar_lm)
-
-#Linear regression per seasons from june dataset
-winter_lm <- lm(price_n ~ bathrooms + accommodates + beds + bedrooms + review_scores_rating + bathroom_shared + hawaii_neighbourhood + kauai_neighbourhood + maui_neighbourhood +entire_home_apt_room_type + private_room_room_type + shared_room_room_type + instant_bookable_dummy + time_diff, merged_june, na.action = na.omit)
-summary(winter_lm)
-
-summer_lm <- lm(price_n ~ bathrooms + accommodates + beds + bedrooms + review_scores_rating + bathroom_shared + hawaii_neighbourhood + kauai_neighbourhood + maui_neighbourhood +entire_home_apt_room_type + private_room_room_type + shared_room_room_type + instant_bookable_dummy + time_diff, merged_june, na.action = na.omit)
-summary(summer_lm)
