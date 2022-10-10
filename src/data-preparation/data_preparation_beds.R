fc417a593af23346c07148599ebc9f1cbc2642bf
Ana Luca <60931541+Ana042@users.noreply.github.com>
2022-10-10 14:14:54 +0200

    Revised code
    corrected mistakes in beds and bedrooms, added file extensions where needed, added code for creating seasons data frames, updating regression analysis for that data

--- src/data-preparation/data_preparation_beds.R
+++ src/data-preparation/data_preparation_beds.R
@@ -3,12 +3,30 @@ dec_s$beds <- ifelse(dec_s$accommodates == 1 & is.na(dec_s$beds), 1, dec_s$beds)
 mar_s$beds <- ifelse(mar_s$accommodates == 1 & is.na(mar_s$beds), 1, mar_s$beds)
 june_s$beds <- ifelse(june_s$accommodates == 1 & is.na(june_s$beds), 1, june_s$beds)
 
-# Remaining NAs are changed into the mean of beds
-dec_s$beds[is.na(dec_s$beds)] <- mean(dec_s$beds,na.rm = TRUE)
-mar_s$beds[is.na(mar_s$beds)] <- mean(mar_s$beds,na.rm = TRUE)
-june_s$beds[is.na(june_s$beds)] <- mean(june_s$beds,na.rm = TRUE)
+# Remaining NAs are changed into the mean of beds per room type
+dec_s$beds[is.na(dec_s$beds) & dec_s$room_type == "Entire home/apt"] <- mean(dec_s$beds[dec_s$room_type == "Entire home/apt"],na.rm = TRUE)
+dec_s$beds[is.na(dec_s$beds) & dec_s$room_type == "Private room"] <- mean(dec_s$beds[dec_s$room_type == "Private room"],na.rm = TRUE)
+dec_s$beds[is.na(dec_s$beds) & dec_s$room_type == "Hotel room"] <- mean(dec_s$beds[dec_s$room_type == "Hotel room"],na.rm = TRUE)
+dec_s$beds[is.na(dec_s$beds) & dec_s$room_type == "Shared room"] <- mean(dec_s$beds[dec_s$room_type == "Shared room"],na.rm = TRUE)
+
+mar_s$beds[is.na(mar_s$beds) & mar_s$room_type == "Entire home/apt"] <- mean(mar_s$beds[mar_s$room_type == "Entire home/apt"],na.rm = TRUE)
+mar_s$beds[is.na(mar_s$beds) & mar_s$room_type == "Private room"] <- mean(mar_s$beds[mar_s$room_type == "Private room"],na.rm = TRUE)
+mar_s$beds[is.na(mar_s$beds) & mar_s$room_type == "Hotel room"] <- mean(mar_s$beds[mar_s$room_type == "Hotel room"],na.rm = TRUE)
+mar_s$beds[is.na(mar_s$beds) & mar_s$room_type == "Shared room"] <- mean(mar_s$beds[mar_s$room_type == "Shared room"],na.rm = TRUE)
+
+june_s$beds[is.na(june_s$beds) & june_s$room_type == "Entire home/apt"] <- mean(june_s$beds[june_s$room_type == "Entire home/apt"],na.rm = TRUE)
+june_s$beds[is.na(june_s$beds) & june_s$room_type == "Private room"] <- mean(june_s$beds[june_s$room_type == "Private room"],na.rm = TRUE)
+june_s$beds[is.na(june_s$beds) & june_s$room_type == "Hotel room"] <- mean(june_s$beds[june_s$room_type == "Hotel room"],na.rm = TRUE)
+june_s$beds[is.na(june_s$beds) & june_s$room_type == "Shared room"] <- mean(june_s$beds[june_s$room_type == "Shared room"],na.rm = TRUE)
+
 
 #check if there are NA's left
 summary(dec_s$beds)
 summary(june_s$beds)
 summary(mar_s$beds)
+
+#take rounded values
+dec_s$beds <- round(dec_s$beds)
+june_s$beds <- round(june_s$beds)
+mar_s$beds <- round(mar_s$beds)
+
