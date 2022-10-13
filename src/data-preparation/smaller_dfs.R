33495833bcc060c1767c47d96c5aaaefc4ab302e
Ana Luca <60931541+Ana042@users.noreply.github.com>
2022-10-13 13:37:08 +0200

    Delete smaller_dfs.R

--- src/data-preparation/smaller_dfs.R
+++
@@ -1,9 +0,0 @@
-#creating smaller df containing only necessary columns of listings tables
-library(tidyverse)
-#summary(june)
-june_s <- june %>% select(id, bathrooms, bathrooms_text, neighbourhood_group_cleansed, room_type, accommodates,
-                          bedrooms, beds, instant_bookable, review_scores_rating)
-mar_s <- mar %>% select(id, bathrooms, bathrooms_text, neighbourhood_group_cleansed, room_type, accommodates,
-                        bedrooms, beds, instant_bookable, review_scores_rating)
-dec_s <- dec %>% select(id, bathrooms, bathrooms_text, neighbourhood_group_cleansed, room_type, accommodates,
-                        bedrooms, beds, instant_bookable, review_scores_rating)
